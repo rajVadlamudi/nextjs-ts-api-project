ab1d09fb2e9789dd31e3ea8aff157369e3f99bbe
rajVadlamudi <vadlamudiraju@gmail.com>
2024-10-19 17:25:08 +0100

    added form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
-
+"use client"
+import { useState, FormEvent } from "react";
 export default function Home() {
+  const [inputVal, updateVal] = useState("") 
+  const handleSubmit = (event:FormEvent) => {
+    event.preventDefault();
+  }
   return (
     <div>
-      Main page
+    <div>
+      <h1>Enter your name:</h1>
+    </div>
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Type ypur name..."
+      value={inputVal}
+      onChange={(e)=> updateVal(e.target.value)} />
+      <button type="submit">Predict Data</button>
+    </form>
     </div>
   );
 }
